861e2d51e72fbca084a6b62d2adda598a9f498cd
Nils Wöhler <ncwoehler@gmail.com>
2019-02-19 17:42:23 +0100

    Start working on homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Group} from "../Group";
+import {GroupService} from "../group.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+    recentGroups: Group[];
+
+    constructor(private groupService: GroupService) {}
+
+    ngOnInit() {
+        this.getRecentGroups();
+    }
+
+    getRecentGroups(): void {
+        this.groupService.getRecentGroups()
+            .subscribe(recent => this.recentGroups = recent);
+    }
 
 }
